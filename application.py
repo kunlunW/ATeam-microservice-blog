65bf327ec7f8bdc2c85ca3ffda24dcdbe476071f
Xuan Li <xl3125@columbia.edu>
2022-12-13 02:02:36 -0500

    modify db structure, add features

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, Response, request
 from datetime import datetime
 import json
-from database_operations import DatabaseOperations, CURRENT_USER_ID
+from database_operations import DatabaseOperations
 from flask_cors import CORS
 import sys
 
@@ -11,53 +11,29 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.get("/api/health")
-def get_health():
-    t = str(datetime.now())
-    msg = {
-        "name": "F22-Starter-Microservice",
-        "health": "Good",
-        "at time": t
-    }
-
-    # DFF TODO Explain status codes, content type, ... ...
-    result = Response(json.dumps(msg), status=200, content_type="application/json")
-
-    return result
-
-
-@app.route("/login", methods=["POST"])
-def check_user_login():
-    print("signed in")
-    content_type = request.headers.get('Content-Type')
-    if content_type == 'application/json':
-        user_login_info = request.json
-        username = user_login_info['username']
-        password = user_login_info['password']
-        result = DatabaseOperations.get_by_key(username, password)
-        return result
-
 @app.route("/createPost", methods=["POST", "GET"])
 def post_blog():
-    if CURRENT_USER_ID is not None:
-        content_type = request.headers.get('Content-Type')
-        if content_type == 'application/json':
-            new_blog = request.json
-            title = new_blog["title"]
-            content = new_blog["description"]
-            tags = new_blog["tag"].split(" ")
-            while("" in tags):
-                tags.remove("")
-            post_time = str(datetime.now())
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        new_blog = request.json
 
-            return DatabaseOperations.new_blog_post(title, content, post_time, tags)
+        owner_id = new_blog['username']
+        title = new_blog["title"]
+        description = new_blog["description"]
+        tags = new_blog["tag"].split(" ")
+        while("" in tags):
+            tags.remove("")
+        post_time = datetime.now().isoformat(sep=" ", timespec="seconds")
+
+        return DatabaseOperations.new_blog_post(owner_id, title, description, post_time, tags)
 
     else: 
         failure_message = {'status': 'fail', 'message': 'Log in required'}
         fail_response = Response(json.dumps(failure_message), status=200, content_type="application.json")
         return fail_response
    
-@app.route("/<owner_id>/posts", methods=["POST", "GET"])
+
+@app.route("/<owner_id>/myposts", methods=["POST", "GET"])
 def get_blog(owner_id):
     result = DatabaseOperations.get_own_post(owner_id)
     if result:
@@ -66,15 +42,82 @@ def get_blog(owner_id):
         response = Response("404 NOT FOUND", status=404, content_type="application/json")
     return response
 
-@app.route("/allposts", methods=["POST", "GET"])
-def get_allblog():
-    result = DatabaseOperations.get_all_post()
+
+@app.route("/allposts", methods=["GET"])
+def get_all_blogs():
+    result = DatabaseOperations.get_all_posts()
     if result:
         response = Response(json.dumps(result, default=str), status=200, content_type="application/json")
     else:
         response = Response("404 NOT FOUND", status=404, content_type="application/json")
     return response
 
+
+@app.route("/posts/<blog_id>", methods=["GET"])
+def get_blog_by_id(blog_id):
+    result = DatabaseOperations.get_blog_by_blogid(blog_id)
+    if result:
+        response = Response(json.dumps(result, default=str), status=200, content_type="application/json")
+    else:
+        response = Response("404 NOT FOUND", status=404, content_type="application/json")
+    return response
+
+
+@app.route("/posts/<blog_id>/likecount", methods=["GET"])
+def get_like_count(blog_id):
+    result = DatabaseOperations.get_like_and_dislike_num(blog_id)
+    result['likestate'] = 0
+    if result:
+        response = Response(json.dumps(result, default=str), status=200, content_type="application/json")
+    else:
+        response = Response("404 NOT FOUND", status=404, content_type="application/json")
+
+    return response
+
+
+@app.route("/<user_id>/posts/<blog_id>/likestate", methods=["GET"])
+def get_like_state(user_id, blog_id):
+    result = DatabaseOperations.like_and_dislike_state_check(user_id, blog_id)
+    if result:
+        response = Response(json.dumps(result, default=str), status=200, content_type="application/json")
+    else:
+        response = Response("404 NOT FOUND", status=404, content_type="application/json")
+
+    return response
+
+@app.route("/<user_id>/posts/<blog_id>/addlike", methods=["GET"])
+def add_like(user_id, blog_id):
+    response = DatabaseOperations.add_to_like(user_id, blog_id)
+    return response
+
+
+@app.route("/<user_id>/posts/<blog_id>/adddislike", methods=["GET"])
+def add_dislike(user_id, blog_id):
+    response = DatabaseOperations.add_to_dislike(user_id, blog_id)
+    return response
+
+
+@app.route("/<user_id>/posts/<blog_id>/removelike", methods=["GET"])
+def remove_like(user_id, blog_id):
+    response = DatabaseOperations.remove_from_like(user_id, blog_id)
+    return response 
+
+
+@app.route("/<user_id>/posts/<blog_id>/removedislike", methods=["GET"])
+def remove_dislike(user_id, blog_id):
+    response = DatabaseOperations.remove_from_dislike(user_id, blog_id)
+    return response 
+
+
+@app.route("/blognumber", methods=["GET"])
+def get_blog_number_by_username():
+    username = request.args.get('username')
+    blog_num = str(DatabaseOperations.get_blog_number(username))
+    response = Response(blog_num, status=200, content_type="application/json")
+    return response
+
+
 if __name__ == "__main__":
     app.debug = True
-    app.run(host="0.0.0.0", port=5011)
+    app.run(host="0.0.0.0", port=5012)
+    
--- database_operations.py
+++ database_operations.py
@@ -1,13 +1,11 @@
-from optparse import TitledHelpFormatter
 #from turtle import title
 import pymysql
 from flask import Flask, Response, request
 import json
-# from flask_mysqldb import MySQL
 import os
 
-#TODO: INIT TO None
-CURRENT_USER_ID = '7d529dd4-548b-4258-aa8e-23e34dc8d43d'
+# #TODO: INIT TO None
+# CURRENT_USER_ID = '7d529dd4-548b-4258-aa8e-23e34dc8d43d'
 
 
 class DatabaseOperations:
@@ -19,7 +17,7 @@ class DatabaseOperations:
     def _get_connection():
 
         usr = "root"
-        pw = "Wd311714@"
+        pw = "84443295412lx."
         h = "localhost"
 
         conn = pymysql.connect(
@@ -31,36 +29,18 @@ class DatabaseOperations:
         )
         return conn
 
-    @staticmethod
-    def get_by_key(first_name, password):
-
-        sql = "SELECT * FROM cs6156_login_microservice.user_info where first_name = %s AND password = %s";
-        conn = DatabaseOperations._get_connection()
-        cur = conn.cursor()
-        res = cur.execute(sql, args=(first_name, password))
-        result = cur.fetchone()
-        if result:
-            CURRENT_USER_ID = result['unique_user_id']
-            found_user_message = {'status': 'success', 'message': 'Successfully found user', 'user_id': CURRENT_USER_ID}
-            found_response = Response(json.dumps(found_user_message), status=200, content_type="application.json")
-            # if credential validation success, update CURRENT_USER_ID
-            return found_response
-        else:
-            failure_message = {'status': 'fail', 'message': 'Failed finding user'}
-            fail_response = Response(json.dumps(failure_message), status=200, content_type="application.json")
-            return fail_response
 
     @staticmethod
-    def new_blog_post(blog_title, blog_content, post_time, tags):
+    def new_blog_post(owner_id, blog_title, blog_content, post_time, tags):
         #init vars
         new_blog_id = ""
         total_post = 0 
 
         #create a new blog id
-        sql = "SELECT owner_id, MAX(total_post) AS max_count FROM cs6156_login_microservice.blog_info WHERE owner_id = %s GROUP BY owner_id";
+        sql = "SELECT owner_id, MAX(total_post) AS max_count FROM blogs.blog_info WHERE owner_id = %s GROUP BY owner_id";
         conn = DatabaseOperations._get_connection()
         cur = conn.cursor()
-        res = cur.execute(sql, args=(CURRENT_USER_ID))
+        res = cur.execute(sql, args=(owner_id))
         result = cur.fetchone()
         tag1 = None if len(tags) == 0 else tags[0]
         tag2 = None if len(tags) <= 1 else tags[1]
@@ -70,15 +50,15 @@ class DatabaseOperations:
             new_blog_id =  result["owner_id"] + "-" + str(int(result["max_count"]) + 1)
             total_post = int(result["max_count"]) + 1
         else:
-            new_blog_id = CURRENT_USER_ID + "-" + str(1)
+            new_blog_id = owner_id + "-" + str(1)
             total_post = 1
      
         #enter new entry into blog_info table
-        sql = "INSERT INTO cs6156_login_microservice.blog_info VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)";
+        sql = "INSERT INTO blogs.blog_info VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, '[]', 0, '[]', 0)"
         conn = DatabaseOperations._get_connection()
 
         cur = conn.cursor()
-        res = cur.execute(sql, args=(new_blog_id, CURRENT_USER_ID, blog_title, blog_content, post_time, total_post, tag1, tag2, tag3))
+        res = cur.execute(sql, args=(new_blog_id, owner_id, blog_title, blog_content, post_time, total_post, tag1, tag2, tag3))
         if res:
             posting_success = {'status': 'success', 'message': 'Successfully Posted'}
             success_response = Response(json.dumps(posting_success), status=200, content_type="application.json")
@@ -88,9 +68,26 @@ class DatabaseOperations:
             fail_response = Response(json.dumps(posting_failed), status=200, content_type="application.json")
             return fail_response
 
+
+    @staticmethod
+    def get_blog_number(username):
+        sql = "SELECT COUNT(*) FROM blogs.blog_info WHERE owner_id=%s"
+        conn = DatabaseOperations._get_connection()
+        cur = conn.cursor()
+
+        try:
+            cur.execute(sql, username)
+        except:
+            return None
+        
+        result = cur.fetchone()
+        result = result["COUNT(*)"]
+        return result    
+
+
     @staticmethod
     def get_own_post(owner_id):
-        sql = "SELECT unique_blog_id as id, blog_title as title, blog_content as description, post_time AS posttime FROM cs6156_login_microservice.blog_info Where owner_id = %s;"
+        sql = "SELECT * FROM blogs.blog_info Where owner_id = %s ORDER BY unique_blog_id DESC;"
         conn = DatabaseOperations._get_connection()
         cur = conn.cursor()
         try:
@@ -100,9 +97,10 @@ class DatabaseOperations:
         result = cur.fetchall()
         return result
     
+
     @staticmethod
-    def get_all_post():
-        sql = "SELECT unique_blog_id as id, blog_title as title, blog_content as description, post_time AS posttime FROM cs6156_login_microservice.blog_info;"
+    def get_all_posts():
+        sql = "SELECT * FROM blogs.blog_info ORDER BY unique_blog_id DESC;"
         conn = DatabaseOperations._get_connection()
         cur = conn.cursor()
         try:
@@ -113,5 +111,165 @@ class DatabaseOperations:
         return result
 
 
-   
+    @staticmethod
+    def get_blog_by_blogid(blog_id):
+        sql = "SELECT * FROM blogs.blog_info WHERE unique_blog_id=%s"
+        conn = DatabaseOperations._get_connection()
+        cur = conn.cursor()
+        try:
+            cur.execute(sql, blog_id)
+        except:
+            return None
+        result = cur.fetchone()
+        return result 
 
+   
+    @staticmethod
+    def get_like_state(user_id, blog_id):
+        conn = DatabaseOperations._get_connection()
+        cur = conn.cursor()
+        sql = "SELECT JSON_SEARCH((select liked_by from blogs.blog_info where unique_blog_id = %s), 'one', %s)"
+
+        cur.execute(sql, (blog_id, user_id))
+        result = list(cur.fetchone().values())[0]
+        if result:
+            return 1
+        else:
+            return 0
+
+
+    @staticmethod
+    def get_dislike_state(user_id, blog_id):
+        conn = DatabaseOperations._get_connection()
+        cur = conn.cursor()
+        sql = "SELECT JSON_SEARCH((select disliked_by from blogs.blog_info where unique_blog_id = %s), 'one', %s)"
+
+        cur.execute(sql, (blog_id, user_id))
+        result = list(cur.fetchone().values())[0]
+        if result:
+            return 1
+        else:
+            return 0
+
+
+    @staticmethod
+    def get_like_and_dislike_num(blog_id):
+        conn = DatabaseOperations._get_connection()
+        cur = conn.cursor()
+        sql = "select likecount, dislikecount from blogs.blog_info where unique_blog_id = %s"
+        cur.execute(sql, blog_id)
+        res = cur.fetchone()
+        return res
+
+
+    @staticmethod
+    def like_and_dislike_state_check(user_id, blog_id):
+
+        res = DatabaseOperations.get_like_and_dislike_num(blog_id)
+        like = DatabaseOperations.get_like_state(user_id, blog_id)
+        dislike = DatabaseOperations.get_dislike_state(user_id, blog_id)
+
+        if like:
+            res['likestate'] = '1'
+        elif dislike:
+            res['likestate'] = '2'
+        else:
+            res['likestate'] = '0'
+        return res
+
+
+    @staticmethod
+    def add_to_like(user_id, blog_id):
+        conn = DatabaseOperations._get_connection()
+        cur = conn.cursor()
+
+        sql = "select json_array_append((select liked_by from blogs.blog_info where unique_blog_id = %s),'$', %s)"  
+        res = cur.execute(sql, (str(blog_id), str(user_id)))
+        added_res = list(cur.fetchone().values())[0]
+
+        update_sql = "update blogs.blog_info set liked_by = %s where unique_blog_id=%s"
+        res = cur.execute(update_sql,(added_res, blog_id))
+
+        plus_sql = "update blogs.blog_info set likecount = likecount + 1 where unique_blog_id=%s"
+        cur.execute(plus_sql, blog_id)
+
+        if res:
+            message = {'status': 'success', 'message': 'like Successfully added!'}
+            response = Response(json.dumps(message), status=200, content_type="application.json")
+            return response   
+        else:
+            message = {'status': 'failed', 'message': 'like failed added!'}
+            response = Response(json.dumps(message), status=200, content_type="application.json")
+            return response 
+
+
+    @staticmethod
+    def add_to_dislike(user_id, blog_id):
+        conn = DatabaseOperations._get_connection()
+        cur = conn.cursor()
+        sql = "select json_array_append((select disliked_by from blogs.blog_info where unique_blog_id=%s),'$', %s)"  
+        cur.execute(sql, (blog_id, user_id))
+        added_res = list(cur.fetchone().values())[0]
+
+        update_sql = "update blogs.blog_info set disliked_by = %s where unique_blog_id=%s"
+        res = cur.execute(update_sql,(added_res, blog_id))
+
+        plus_sql = "update blogs.blog_info set dislikecount = dislikecount + 1 where unique_blog_id=%s"
+        cur.execute(plus_sql, blog_id)
+
+        if res:
+            message = {'status': 'success', 'message': 'dislike Successfully added!'}
+            response = Response(json.dumps(message), status=200, content_type="application.json")
+            return response 
+        else:
+            message = {'status': 'failed', 'message': 'dislike failed added!'}
+            response = Response(json.dumps(message), status=200, content_type="application.json")
+            return response 
+
+
+    @staticmethod
+    def remove_from_like(user_id, blog_id):
+        conn = DatabaseOperations._get_connection()
+        cur = conn.cursor()
+        sql = "select json_remove((select liked_by from blogs.blog_info where unique_blog_id = %s), JSON_UNQUOTE((SELECT JSON_SEARCH((select liked_by from blogs.blog_info where unique_blog_id = %s), 'one', %s))))"
+        cur.execute(sql, (blog_id, blog_id, user_id))
+        removed_res = list(cur.fetchone().values())[0]
+
+        update_sql = "update blogs.blog_info set liked_by = %s where unique_blog_id=%s"
+        res = cur.execute(update_sql,(removed_res, blog_id))
+
+        minus_sql = "update blogs.blog_info set likecount = likecount - 1 where unique_blog_id=%s"
+        cur.execute(minus_sql, blog_id)
+
+        if res:
+            message = {'status': 'success', 'message': 'like Successfully removed!'}
+            response = Response(json.dumps(message), status=200, content_type="application.json")
+            return response 
+        else:
+            message = {'status': 'failed', 'message': 'like failed removed!'}
+            response = Response(json.dumps(message), status=200, content_type="application.json")
+            return response 
+
+
+    @staticmethod
+    def remove_from_dislike(user_id, blog_id):
+        conn = DatabaseOperations._get_connection()
+        cur = conn.cursor()
+        sql = "select json_remove((select disliked_by from blogs.blog_info where unique_blog_id = %s), JSON_UNQUOTE((SELECT JSON_SEARCH((select disliked_by from blogs.blog_info where unique_blog_id = %s), 'one', %s))))"
+        cur.execute(sql, (blog_id, blog_id, user_id))
+        removed_res = list(cur.fetchone().values())[0]
+
+        update_sql = "update blogs.blog_info set disliked_by = %s where unique_blog_id=%s"
+        res = cur.execute(update_sql,(removed_res, blog_id))
+
+        minus_sql = "update blogs.blog_info set dislikecount = dislikecount - 1 where unique_blog_id=%s"
+        cur.execute(minus_sql, blog_id)
+
+        if res:
+            message = {'status': 'success', 'message': 'dislike Successfully removed!'}
+            response = Response(json.dumps(message), status=200, content_type="application.json")
+            return response 
+        else:
+            message = {'status': 'failed', 'message': 'dislike failed removed!'}
+            response = Response(json.dumps(message), status=200, content_type="application.json")
+            return response 
